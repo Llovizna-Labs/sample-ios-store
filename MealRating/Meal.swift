e83767bf724a4bd3bfa977c00c695b4348bc135b
Ronald Suez <rsuez93@gmail.com>
2016-05-03 07:31:39 -0430

    added project files

---
+++ MealRating/Meal.swift
@@ -0,0 +1,38 @@
+//
+//  Meal.swift
+//  MealRating
+//
+//  Created by Ronald J. Suez A. on 5/3/16.
+//  Copyright © 2016 lloviznalabs. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+    
+
+}
+
+
---
+++ MealRating/MealTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MealTableViewCell.swift
+//  MealRating
+//
+//  Created by Ronald J. Suez A. on 5/2/16.
+//  Copyright © 2016 lloviznalabs. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var mealImage: UIImageView!
+    @IBOutlet weak var mealName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
