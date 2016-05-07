6aa7129fa12e6d2ee5de5855ddfd15c5ba23d450
Ronald Suez <rsuez93@gmail.com>
2016-05-07 15:59:04 -0430

    added custom title to detail view

--- MealRating/ViewController.swift
+++ MealRating/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.title = meal!.name
         mealTitle.text = meal!.name
         mealImage.image = meal!.photo
         // Do any additional setup after loading the view, typically from a nib.
